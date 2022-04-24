7807774f86284b404ceae2b5637cd8b6327ce671
LaupWing <loc_strict@hotmail.com>
2022-04-24 16:51:59 +0200

    udpate

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const scoreElem = document.querySelector("[data-score]")
 const startScreenElem = document.querySelector("[data-start-screen]")
 const gweiTotalScoreEleme = document.querySelector("[data-wei-total-score]")
 const nftTotalScoreElem = document.querySelector("[data-nft-total-score]")
+
+setPixelToGameScale()
+window.addEventListener('resize', setPixelToGameScale)
+
+let lastTime
+let speedScale
+let score
+let nftScore = 0
+
+function setPixelToGameScale(){
+   let gameToPixelScale
+
+   if(window.innerWidth/window.innerHeight < GAME_WIDTH/ GAME_HEIGHT){
+      gameToPixelScale = window.innerWidth / GAME_WIDTH
+   }else{
+      gameToPixelScale = window.innerHeight / GAME_HEIGHT
+   }
+
+   gameElem.style.width = `${GAME_WIDTH  * gameToPixelScale}px`
+   gameElem.style.height = `${GAME_HEIGHT  * gameToPixelScale}px`
+}
