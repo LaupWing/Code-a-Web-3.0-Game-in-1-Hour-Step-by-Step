7ed85992a062a99ce5369be0e8910295eb9582e1
LaupWing <loc_strict@hotmail.com>
2022-04-24 16:48:35 +0200

    queryseelctors

---
+++ script.js
@@ -0,0 +1,9 @@
+const GAME_WIDTH = 100
+const GAME_HEIGHT = 30
+const SPEED_SCALE_INCREASE = 0.00001
+
+const gameElem = document.querySelector("[data-game]")
+const scoreElem = document.querySelector("[data-score]")
+const startScreenElem = document.querySelector("[data-start-screen]")
+const gweiTotalScoreEleme = document.querySelector("[data-wei-total-score]")
+const nftTotalScoreElem = document.querySelector("[data-nft-total-score]")
