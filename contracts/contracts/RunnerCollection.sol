12a4fdf926b170539ba8c004c3dbb8391af86dcd
LaupWing <loc_strict@hotmail.com>
2022-04-26 15:29:44 +0200

    HARDhat configh

--- contracts/contracts/RunnerCollection.sol
+++ contracts/contracts/RunnerCollection.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+
 contract RunnerCollection is ERC1155 {
     string public name;
     string public symbol;
